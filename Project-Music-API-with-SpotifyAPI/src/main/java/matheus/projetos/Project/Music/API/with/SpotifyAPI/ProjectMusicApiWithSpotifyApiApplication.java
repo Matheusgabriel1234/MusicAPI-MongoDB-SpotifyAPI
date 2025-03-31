cb9547923374ed9f59a148b08471788191d06a98
Matheusgabriel1234 <magaje2005@gmail.com>
2025-03-31 14:22:09 -0300

    Initial Commit

---
+++ Project-Music-API-with-SpotifyAPI/src/main/java/matheus/projetos/Project/Music/API/with/SpotifyAPI/ProjectMusicApiWithSpotifyApiApplication.java
@@ -0,0 +1,13 @@
+package matheus.projetos.Project.Music.API.with.SpotifyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMusicApiWithSpotifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMusicApiWithSpotifyApiApplication.class, args);
+	}
+
+}
---
+++ Project-Music-API-with-SpotifyAPI/src/test/java/matheus/projetos/Project/Music/API/with/SpotifyAPI/ProjectMusicApiWithSpotifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package matheus.projetos.Project.Music.API.with.SpotifyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMusicApiWithSpotifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
