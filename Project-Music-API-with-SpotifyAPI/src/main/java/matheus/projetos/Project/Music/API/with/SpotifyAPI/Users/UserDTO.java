d1db497db05c9986b63f8a42bf98454afd21f4c4
Matheusgabriel1234 <magaje2005@gmail.com>
2025-04-02 14:20:17 -0300

    UserService with DTOs

--- Project-Music-API-with-SpotifyAPI/src/main/java/matheus/projetos/Project/Music/API/with/SpotifyAPI/Users/UserDTO.java
+++ Project-Music-API-with-SpotifyAPI/src/main/java/matheus/projetos/Project/Music/API/with/SpotifyAPI/Users/UserDTO.java
@@ -1,27 +1,17 @@
 package matheus.projetos.Project.Music.API.with.SpotifyAPI.Users;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collection = "User")
-public class UserEntity{
-
-    
-    @Id
+public class UserDTO {
     private String id;
-    private String userName;
+    private String username;
     private String email;
     private String password;
-
-
 }
---
+++ Project-Music-API-with-SpotifyAPI/src/main/java/matheus/projetos/Project/Music/API/with/SpotifyAPI/Users/UserService.java
@@ -0,0 +1,57 @@
+package matheus.projetos.Project.Music.API.with.SpotifyAPI.Users;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+@Autowired
+private UserRepository userRepository;
+
+
+
+public UserDTO createUser(UserDTO userDTO){
+    UserEntity user = convertToEntity(userDTO);
+   UserEntity savedUser = userRepository.save(user);
+   return  convertToDto(savedUser);
+}
+
+
+public UserDTO findByUserById(String id){
+    return userRepository.findById(id).map(this::convertToDto).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+}
+
+public UserDTO update(String id, UserDTO user){
+   UserEntity existingUser = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario não existe"));
+   convertToDto(existingUser);
+   existingUser.setUserName(user.getUsername());
+   existingUser.setEmail(user.getEmail());
+   UserEntity savedUser = userRepository.save(existingUser);
+   return convertToDto(savedUser);
+}
+
+public void deleteUser(String id){
+    userRepository.deleteById(id);
+}
+
+
+private UserDTO convertToDto(UserEntity user){
+    UserDTO userDTO = new UserDTO();
+    userDTO.setEmail(user.getEmail());
+    userDTO.setUsername(user.getUserName());
+    userDTO.setPassword(user.getPassword());
+    return userDTO;
+}
+
+private UserEntity convertToEntity(UserDTO userDTO){
+    UserEntity userEntity = new UserEntity();
+    userEntity.setUserName(userDTO.getUsername());
+    userEntity.setEmail(userDTO.getEmail());
+    userEntity.setPassword(userDTO.getPassword());
+    return userEntity;
+}
+}
